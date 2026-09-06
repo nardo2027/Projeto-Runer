e94f44bdb1241657389d2ba737cf92bd98bb50ae
nardo2027 <nardo2027@gmail.com>
2026-09-06 14:31:15 -0300

    fix: detect obstacles without requiring Unity tags

--- Assets/Scripts/RunnerCollision.cs
+++ Assets/Scripts/RunnerCollision.cs
@@ -4,13 +4,13 @@ public class RunnerCollision : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Obstacle"))
+        if (collision.gameObject.GetComponent<ObstacleMarker>() != null)
             RunGameManager.Instance?.EndRun();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Obstacle"))
+        if (other.GetComponent<ObstacleMarker>() != null)
             RunGameManager.Instance?.EndRun();
     }
 }
