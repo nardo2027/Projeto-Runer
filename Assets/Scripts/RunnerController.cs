3ec008e43dfee715e48339ca675018a8494314c7
nardo2027 <nardo2027@gmail.com>
2026-09-06 14:31:42 -0300

    feat: add runner jump controls

---
+++ Assets/Scripts/RunnerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RunnerController : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private float groundCheckDistance = 1.15f;
+
+    private Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        body.constraints = RigidbodyConstraints.FreezePositionX |
+                           RigidbodyConstraints.FreezePositionZ |
+                           RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void Update()
+    {
+        if (RunGameManager.Instance == null || !RunGameManager.Instance.IsRunning)
+            return;
+
+        bool jumpPressed = Input.GetKeyDown(KeyCode.Space) ||
+                           Input.GetKeyDown(KeyCode.UpArrow) ||
+                           Input.GetKeyDown(KeyCode.W);
+
+        if (jumpPressed && IsGrounded())
+            Jump();
+    }
+
+    private void Jump()
+    {
+        Vector3 velocity = body.velocity;
+        velocity.y = 0f;
+        body.velocity = velocity;
+        body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 origin = transform.position + Vector3.up * 0.05f;
+        return Physics.Raycast(
+            origin,
+            Vector3.down,
+            groundCheckDistance,
+            Physics.DefaultRaycastLayers,
+            QueryTriggerInteraction.Ignore);
+    }
+}
